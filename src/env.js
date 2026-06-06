b611159dd8ff9e0bd8291be78d5ade9346fd184d
Shubham <gharatshubham306@gmail.com>
2026-06-07 03:48:54 +0530

    Updated env variables

--- src/env.js
+++ src/env.js
@@ -8,7 +8,6 @@ export const env = createEnv({
    */
   server: {
     BETTER_AUTH_SECRET: z.string(),
-    BETTER_AUTH_URL: z.string(),
     DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
@@ -42,7 +41,6 @@ export const env = createEnv({
    */
   runtimeEnv: {
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
-    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     MODAL_KEY:process.env.MODAL_KEY,
