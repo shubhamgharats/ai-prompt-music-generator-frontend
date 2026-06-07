fb3cb065f491a1c6d533791b11d0d3b9d65f0ad6
Shubham <gharatshubham306@gmail.com>
2026-06-07 19:39:59 +0530

    fix inngest route and server setup

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,7 +1,10 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { generateSong } from "./function";
+// Create an API that serves zero functions
+export const runtime = "nodejs";
 
-export const { GET, POST, PUT } = serve(inngest, [
-  generateSong,
-]);
+export const { GET, POST } = serve({
+  client: inngest,
+  functions: [generateSong],
+});
