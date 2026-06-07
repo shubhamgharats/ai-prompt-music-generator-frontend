8c701f1efa7c8a90f6c6d41c3d53c309131ab99e
Shubham <gharatshubham306@gmail.com>
2026-06-07 19:26:09 +0530

    fix inngest handler

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,10 +1,7 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { generateSong } from "./function";
-// Create an API that serves zero functions
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [
-generateSong
-   ],
-});
+
+export const { GET, POST, PUT } = serve(inngest, [
+  generateSong,
+]);
