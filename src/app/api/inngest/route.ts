7d7714bade3d42f9cade247ce9db596b583534a4
shubhamgharats <gharatshubham306@gmail.com>
2026-06-08 10:50:11 +0530

    Fix import path for generateSong function

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { generateSong } from "./function";
+import { generateSong } from "../../../inngest/function";
 // Create an API that serves zero functions
 export const runtime = "nodejs";
 
